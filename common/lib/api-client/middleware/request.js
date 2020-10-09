d35f91dca7cb2b761c121c92c3f9e813e2ed8f82
Alex Robinson <github@alex.fu2k.org>
2020-10-09 12:03:02 +0100

    feat: Add keep-alive to api client

--- common/lib/api-client/middleware/request.js
+++ common/lib/api-client/middleware/request.js
@@ -1,4 +1,15 @@
+const HttpAgent = require('agentkeepalive')
+const axios = require('axios')
 const debug = require('debug')('app:api-client:axios-request')
+
+const { HttpsAgent } = HttpAgent
+
+const axiosInstance = axios.create({
+  httpAgent: new HttpAgent(),
+  httpsAgent: new HttpsAgent(),
+  // follow up to 10 HTTP 3xx redirects
+  maxRedirects: 10,
+})
 
 const cache = require('../cache')
 
@@ -10,12 +21,11 @@ function requestMiddleware({ cacheExpiry = 60, useRedisCache = false } = {}) {
         return payload.res
       }
 
-      const { req, jsonApi, cacheKey } = payload
+      const { req, cacheKey } = payload
 
       debug('API REQUEST', req.url)
 
-      const response = await jsonApi
-        .axios(req)
+      const response = await axiosInstance(req)
         .then(async response => {
           if (cacheKey) {
             debug('CACHEING API RESPONSE', cacheKey)
